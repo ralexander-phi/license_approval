23297d8d7d4bf67ecc35587dbf4ebc29ae215dd2
Robert Alexander <ralexander-phi>
2019-10-18 15:33:31 -0400

    Install matching bundler version

--- script.sh
+++ script.sh
@@ -8,7 +8,11 @@ cat Gemfile
 echo "Gemfile.lock"
 cat Gemfile.lock
 
-# make sure dependencies are installed
+# Install matching bundler version for max compatibility
+BUNDLER_VERSION=$(cat Gemfile.lock | grep -A 1 "BUNDLED WITH" | egrep "^\s+[0-9]+(\.[0-9]+)*$")
+gem install bundler -v $BUNDLER_VERSION
+
+# Make sure dependencies are installed
 bundle install
 
 # Run the tool
