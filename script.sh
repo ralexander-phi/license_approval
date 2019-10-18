8085442984a2f09063ecd459627c32050928bb3f
Robert Alexander <ralexander-phi>
2019-10-18 14:47:11 -0400

    Catch error better

--- script.sh
+++ script.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
+# Run the tool
 cd "$GITHUB_WORKSPACE"
-REPORT=$(license_finder report --format json | jq -c)
+REPORT=$(license_finder report --format json)
+if [ "$?" -ne 0 ]; then
+    echo "Could not run the report tool!"
+	echo "$REPORT"
+	exit 1
+fi
 
-echo ::set-output name=license_report_json::$REPORT
+# minimize the JSON
+REPORT=$(echo $REPORT | jq -c)
+
+# set it for Github Actions to read as an output
+echo "::set-output name=license_report_json::$REPORT"
