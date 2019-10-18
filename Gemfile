8926bd9e6a17709cc000826c6f80d582f037206c
Robert Alexander <ralexander-phi>
2019-10-18 14:33:08 -0400

    fixing

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'license_finder'
+gem 'bundler'
 
--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd "$GITHUB_WORKSPACE"
-REPORT=$(license_finder report --format json)
+REPORT=$(license_finder report --format json | jq -c)
 
 echo ::set-output name=license_report_json::$REPORT
