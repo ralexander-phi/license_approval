909a098a33afd59c2fcc151f89ee4b0852a76a14
Robert Alexander <ralexander-phi>
2019-10-18 21:38:26 -0400

    cleanup

--- script.sh
+++ script.sh
@@ -2,12 +2,6 @@
 
 cd "$GITHUB_WORKSPACE"
 
-echo "Gemfile"
-cat Gemfile
-
-echo "Gemfile.lock"
-cat Gemfile.lock
-
 # Install matching bundler version for max compatibility
 BUNDLER_VERSION=$(cat Gemfile.lock | grep -A 1 "BUNDLED WITH" | egrep "^\s+[0-9]+(\.[0-9]+)*$")
 gem install bundler -v $BUNDLER_VERSION
@@ -15,8 +9,12 @@ gem install bundler -v $BUNDLER_VERSION
 # Make sure dependencies are installed
 bundle install
 
+#
 # Run the tool
-echo "Running license_finder"
+#
+# This inspects all the dependencies, and determines their version and licenses
+# JSON output is used for further steps to consume
+#
 REPORT=$(license_finder report --format json)
 if [ "$?" -ne 0 ]; then
     echo "Could not run the report tool!"
@@ -24,15 +22,16 @@ if [ "$?" -ne 0 ]; then
 	exit 1
 fi
 
-echo "DONE. Got: $REPORT"
-
-# Minimize
-echo "minimize"
+#
+# Minimize JSON
+#
+# Remove all output until the start of the JSON (I.E. "{" char)
+#    license_finder can be chatty
+# Base64 is used to ensure we don't have issues with special characters
+# Condense to a single line
+#
 REPORT=$(echo $REPORT | sed 's/[^{]*{/{/' | base64 | tr -d '\n')
-echo "Done. Got: $REPORT"
 
-# set it for Github Actions to read as an output
+# Special output for Github Actions to read the value
 echo "##[set-output name=license_report_json;]$REPORT"
-
-echo "##[set-output name=version;]debug"
 
