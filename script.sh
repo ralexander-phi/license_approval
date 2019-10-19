b5069690713cbfc8505233869c5e8bca7030d768
Robert Alexander <ralexander-phi>
2019-10-18 22:00:28 -0400

    descriptive

--- script.sh
+++ script.sh
@@ -2,12 +2,26 @@
 
 cd "$GITHUB_WORKSPACE"
 
-# Install matching bundler version for max compatibility
-BUNDLER_VERSION=$(cat Gemfile.lock | grep -A 1 "BUNDLED WITH" | egrep "^\s+[0-9]+(\.[0-9]+)*$")
-gem install bundler -v $BUNDLER_VERSION
+if [ -f Gemfile.lock ]; then
+	# Install matching bundler version for max compatibility
+	BUNDLER_VERSION=$(cat Gemfile.lock | grep -A 1 "BUNDLED WITH" | egrep "^\s+[0-9]+(\.[0-9]+)*$")
+	if [ "$?" -eq 0 ]; then
+		echo "Installing bundler version $BUNDLER_VERSION, to match Gemfile.lock"
+		gem install bundler -v $BUNDLER_VERSION
+		if [ "$?" -ne 0 ]; then
+			echo "Failed to install matching bundler version."
+			echo "Trying with inconsistent bundler version anyway."
+		fi
+	fi
+fi
 
 # Make sure dependencies are installed
 bundle install
+if [ "$?" -ne 0 ]; then
+	echo "Failed to install dependencies"
+	echo "Try to pre-install these before calling this action"
+	exit 1
+fi
 
 #
 # Run the tool
@@ -17,9 +31,9 @@ bundle install
 #
 REPORT=$(license_finder report --format json)
 if [ "$?" -ne 0 ]; then
-    echo "Could not run the report tool!"
-    echo "$REPORT"
-    exit 1
+	echo "Could not run the report tool!"
+	echo "$REPORT"
+	exit 1
 fi
 
 #
