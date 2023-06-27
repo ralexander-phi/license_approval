1432835dd59d8fb08aa4507ec222880b41ee4fe5
Robert Alexander <3751418+ralexander-phi@users.noreply.github.com>
2023-06-27 06:07:37 -0400

    Update versions, get rid of JSON output (#10)
    Update all the versions.
    
    JSON output isn't used and has the unfortunate effect of allowing the job to succeed. Removed.

--- script.sh
+++ script.sh
@@ -23,29 +23,5 @@ if [ "$?" -ne 0 ]; then
 	exit 1
 fi
 
-#
-# Run the tool
-#
-# This inspects all the dependencies, and determines their version and licenses
-# JSON output is used for further steps to consume
-#
-REPORT=$(license_finder report --format json)
-if [ "$?" -ne 0 ]; then
-	echo "Could not run the report tool!"
-	echo "$REPORT"
-	exit 1
-fi
-
-#
-# Minimize JSON
-#
-# Remove all output until the start of the JSON (I.E. "{" char)
-#    license_finder can be chatty
-# Base64 is used to ensure we don't have issues with special characters
-# Condense to a single line
-#
-REPORT=$(echo $REPORT | sed 's/[^{]*{/{/' | base64 | tr -d '\n')
-
-# Special output for Github Actions to read the value
-echo "##[set-output name=license_report_json;]$REPORT"
+license_finder
 
